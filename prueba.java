8cfa7ad2867702f5956dc4db3ca0447316a8a5b2
Alejandro Manotas <manotasja@uninorte.edu.co>
2020-10-04 20:07:58 -0500

    Update prueba.java

--- prueba.java
+++ prueba.java
@@ -1,18 +1,20 @@
 public class AnyThing{
-public static void main (String[] args){
-int i=1;
-int j=20;
-char c;
-String cadena;
-double z;
-z=14.9e-8;
-z=12.9;
-cadena="Hola";
-c="a";
-i=i*2;
-for (int k = 1; k < j; k++){
-i*=k;
-z/=2.0;
-}
-}
+  public static void main (String[] args){
+    int i=1;
+    int j=20; //Este comentario no esta incluido en el archivo de salida
+    int no = 54; //Esta variable tampoco
+    int si = "hola como estas"; //Esta variable tampoco
+    char c;
+    String cadena;
+    double z;
+    z=14.9e-8;
+    z=12.9;
+    cadena="Hola";
+    c="a";
+    i=i*2;
+    for (int k = 1; k < j; k++){
+      i*=k;
+      z/=2.0;
+    }
+  }
 }
